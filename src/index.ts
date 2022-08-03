f5f8a283cf7717922ba0ce8a9835d5844ce5bf72
sorrynthx <sorrynthx@gmail.com>
2022-08-03 19:03:03 +0900

    2022-08-03 타입스크립트 블록체인 -> 블록생성 2 + typescript 노드 설치 및 타입스크립트로 작성된 라이브러리 찾는 방법

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ class Block implements BlockShape {
         public height: number,
         public data: string
     ) {
-        this.hash = calculateHash(prevHash, height, data);
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
 
     static calculateHash(prevHash:string, height:number, data:string){
         const toHash = `${prevHash}${height}${data}`;
-        
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
 
