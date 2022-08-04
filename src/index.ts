11c9fc87cbc3d0845a39e4430ceb9d61776178a2
sorrynthx <sorrynthx@gmail.com>
2022-08-04 22:23:45 +0900

    2022-08-04 타입스크립트 블록체인 -> 블록생성 3 + 가짜 데이터 추가 방지 (끝)

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,36 @@ class Block implements BlockShape {
 }
 
 
+class BlockChain {
+    private blocks: Block[]
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if (this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length-1].hash;
+    }
+    public addBlock(data:string) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        // 중간에 데이터 해킹 방지
+        return [...this.blocks];
+    }
+}
+
+
+const blockchain = new BlockChain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+// 블록 가져올 때 가짜 데이터 추가
+blockchain.getBlocks().push(new Block("HEHEHE", 111, "IT IS HACKED BY KKKK"));
+
+console.log(blockchain.getBlocks()); 
+
+
+// npm run build
+// npm start
